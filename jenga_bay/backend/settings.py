c058f13f37e6c5d5f727cdbc16bb28fe7b49355f
JK11751 <kgimase@gmail.com>
2024-08-06 13:17:49 +0300

    render added

--- jenga_bay/backend/settings.py
+++ jenga_bay/backend/settings.py
@@ -9,10 +9,16 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
 import dj_database_url
 
 from pathlib import Path
 import os
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
