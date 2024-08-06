fec990c1ee43e3029b1dd88a649340738c500fc2
JK11751 <kgimase@gmail.com>
2024-08-06 14:07:19 +0300

    super user on render

--- jenga_bay/build.sh
+++ jenga_bay/build.sh
@@ -7,6 +7,11 @@ pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
+#create superuser
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
 
 # Apply any outstanding database migrations
 python manage.py migrate
